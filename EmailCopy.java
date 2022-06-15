5e8aaf7294a6051ca1bc8d061b054b6d1e67f574
Aydan Minor <43589878+aydanminor11@users.noreply.github.com>
2022-06-15 16:34:30 -0400

    Update EmailCopy.java
    v1.1

--- EmailCopy.java
+++ EmailCopy.java
@@ -6,16 +6,16 @@ public class EmailCopy{
   
   public static void main(int argv, String[] args) {
     String myString = "";
-    int need = 0;
-    need = args[0];
+    int need = args[0];
     String name = args[1];
-    String place = args[2];
+    String city = args[2];
+    String state = args[3];
     switch(need) {
-      case 1: myString = "Trying to get an arrival time to see if " + name + " made it into " + place + "?\n\n Thank you,\n";
+      case 1: myString = "Trying to get an arrival time to see if " + name + " made it into " + city + ", " + state + "?\n\n Thank you,\n";
               break;
-      case 2: myString = "Trying to get an update to see if " + name + " got loaded over in " + place + "? If so, could I get the departure time, current temps, and seal number please.\n\n Thank you,\n";
+      case 2: myString = "Trying to get an update to see if " + name + " got loaded over in " + city + ", " + state + "? If so, could I get the departure time, current temps, and seal number please.\n\n Thank you,\n";
               break;
-      case 3:  myString = "Trying to get an update to see if " + name + " was unloaded in " + place + "? If so, could I get the departure time, and did they have clean bills or lumper fees?\n\n Thank you,\n";
+      case 3:  myString = "Trying to get an update to see if " + name + " was unloaded in " + city + ", " + state + "? If so, could I get the departure time, and did they have clean bills or lumper fees?\n\n Thank you,\n";
                break;
       default: myString = "Invalid";
                break;
